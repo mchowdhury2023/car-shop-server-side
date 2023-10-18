a4bfbf7c23b459c3032d55c2b11e506c62fc4152
mchowdhury2023 <chowdhury7135@saskpolytech.ca>
2023-10-18 02:21:17 -0600

    worked on to get brand name from product table, then worked on get method and put method

--- index.js
+++ index.js
@@ -14,6 +14,108 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.psdu9fg.mongodb.net/?retryWrites=true&w=majority`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const productCollection = client.db('BrandShopDB').collection('products');
+
+        // Get unique brands
+        app.get('/brands', async (req, res) => {
+            const cursor = productCollection.aggregate([
+                {
+                    $group: {
+                        _id: "$brandName",
+                        photo: { $first: "$photo" } 
+                    }
+                }
+            ]);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        //get all products of a specific brand
+        app.get('/products/byBrand/:brandName', async (req, res) => {
+            const brand = req.params.brandName;
+            const query = { brandName: brand };
+            const products = await productCollection.find(query).toArray();
+            res.send(products);
+        });
+        
+
+
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedProduct = req.body;
+            const Coffee = {
+                $set: {
+                    name: updatedProduct.name,
+                    quantity: updatedProduct.quantity,
+                    supplier: updatedProduct.supplier,
+                    taste: updatedProduct.taste,
+                    category: updatedProduct.category,
+                    details: updatedProduct.details,
+                    photo: updatedProduct.photo
+                }
+            }
+            const result = await productCollection.updateOne(query, Coffee, options);
+            res.send(result);
+        })
+
+
+
+
+
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await brandCollection.insertOne(newProduct);
+            res.send(result);
+        })
+
+        // app.delete('/products/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) };
+        //     const result = await brandCollection.deleteOne(query);
+        //     res.send(result);
+        // })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
