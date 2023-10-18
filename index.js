b5418c1f8c0f15001049a57f8c7654422473a434
mchowdhury2023 <chowdhury7135@saskpolytech.ca>
2023-10-18 13:30:13 -0600

    worked on to get products based on brands

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-// console.log(process.env.DB_USER)
-// console.log(process.env.DB_PASSWORD)
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.psdu9fg.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -86,10 +83,6 @@ async function run() {
             const result = await productCollection.updateOne(query, Coffee, options);
             res.send(result);
         })
-
-
-
-
 
         app.post('/products', async (req, res) => {
             const newProduct = req.body;
